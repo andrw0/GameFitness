549540935936d1a7e5a2fe19f944b30a11c725b8
Andrew <andrew.shults@cgu.edu>
2019-11-06 11:49:05 -0800

    added models and forms for gamelist

--- src/gamefitnessweb/gamefitnessweb/forms.py
+++ src/gamefitnessweb/gamefitnessweb/forms.py
@@ -1,5 +1,6 @@
+from django import forms
 from django.forms import ModelForm
-from gamefitnessweb.models import ModelForm 
+from gamefitnessweb.models import users, games
 
 # Create the form class
 class UserForm(ModelForm):
@@ -8,10 +9,18 @@ class UserForm(ModelForm):
         fields = ['password','first_name','last_name','email_address','height','weight','gender']
 
 # Create a form to add a user info
-    form = UserForm()
+form = UserForm()
 
 # Create a form to change an existing user information
-    userInfo = users.objects.get(pk=1)
-    form = UserForm(instance=userInfo)
-    
+userInfo = users.objects.get(pk=1)
+form = UserForm(instance=userInfo)
 
+class GameForm(ModelForm):
+    class Meta:
+        model = games
+        fields = ['game_id']
+
+form = GameForm()
+
+gameInfo = users.objects.get(pk=1)
+form = GameForm(instance=gameInfo)
--- src/gamefitnessweb/gamefitnessweb/views.py
+++ src/gamefitnessweb/gamefitnessweb/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
-from .forms import UserForm
+from .forms import UserForm, GameForm
 
 def get_userForm(request):
     # if this is a POST request we need to process the form data
@@ -24,3 +23,22 @@ def get_userForm(request):
 
 def homepage(request):
     return render(request,'homepage.html')
+
+def get_gameForm(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = UserForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            form.save()
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponse("Thank You")
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = UserForm()
+
+    return render(request, '.gameshtml', {'form': form})
