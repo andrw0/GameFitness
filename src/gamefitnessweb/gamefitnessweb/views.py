c76752af51470749c5d941b98d1891d05d6ebf43
Andrew <andrew.shults@cgu.edu>
2019-11-21 15:27:32 -0800

    pulled the queried data from the games table to html

--- src/gamefitnessweb/gamefitnessweb/views.py
+++ src/gamefitnessweb/gamefitnessweb/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from django.shortcuts import render
 from .forms import UserForm, GameForm, ExercisesForm, FeedbackForm
+from .models import games
 
 def showUserForm(request):
     # if this is a POST request we need to process the form data
@@ -26,23 +27,26 @@ def homepage(request):
     return render(request,'./gamefitnessweb/homepage.html')
 
 def showGameForm(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = GameForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            form.save()
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponse("Thank You")
+    form = GameForm()
+    game = games.objects.all()
+    args = {'form': form, 'game':game}
+    # # if this is a POST request we need to process the form data
+    # if request.method == 'POST':
+    #     # create a form instance and populate it with data from the request:
+    #     form = GameForm(request.POST)
+    #     # check whether it's valid:
+    #     if form.is_valid():
+    #         form.save()
+    #         # process the data in form.cleaned_data as required
+    #         # ...
+    #         # redirect to a new URL:
+    #         return HttpResponse("Thank You")
+    #
+    # # if a GET (or any other method) we'll create a blank form
+    # else:
+    #     form = GameForm()
 
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = GameForm()
-
-    return render(request, './gamefitnessweb/games.html', {'form': form})
+    return render(request, './gamefitnessweb/games.html', args)
 
 def showExercisesForm(request):
     # if this is a POST request we need to process the form data
