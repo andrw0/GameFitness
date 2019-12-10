cc1653282b281b6e7ab4d1e3c670df901b69a05b
Andrew <andrew.shults@cgu.edu>
2019-12-10 09:42:04 -0800

    fixed the test_views file

--- src/gamefitnessweb/gamefitnessweb/tests/test_views.py
+++ src/gamefitnessweb/gamefitnessweb/tests/test_views.py
@@ -5,10 +5,28 @@ from gamefitnessweb.views import *
 from mixer.backend.django import mixer
 import pytest
 
-
+@pytest.mark.django_db
 class TestViews():
     def test_homepage(self):
         path = reverse('homepage')
         request = RequestFactory().get(path)
         response = homepage(request)
+        assert response.status_code == 200
+
+    def test_feedback(self):
+        path = reverse('feedback')
+        request = RequestFactory().get(path)
+        response = showFeedbackForm(request)
+        assert response.status_code == 200
+
+    def test_games(self):
+        path = reverse('games')
+        request = RequestFactory().get(path)
+        response = showGameForm(request)
+        assert response.status_code == 200
+
+    def test_exercises(self):
+        path = reverse('exercisesList')
+        request = RequestFactory().get(path)
+        response = showExercisesForm(request)
         assert response.status_code == 200
